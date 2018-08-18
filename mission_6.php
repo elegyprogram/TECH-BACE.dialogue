6d9a187125fdd982dfdf1c3d0b896436374da704
elegyprogram <42497706+elegyprogram@users.noreply.github.com>
2018-08-19 01:41:44 +0900

    Add files via upload

---
+++ mission_6.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja" >
+  <head>
+    <meta charset="utf-8">
+    <title>TECH-BACE.dialogue</title>
+    <link rel="stylesheet" herf="mission_6.css">
+  </head>
+  <body>
+    <div class="header">
+      <a href="url">TECH-BACE</a>
+      <a href="url">通知</a>
+      <a href="url">質問する</a>
+      <a href="url">検索する</a>
+    </div>
+    <div class="right-wrapper">
+      <h1>スレッド一覧</h1>
+      <?php
+        for($i=1;$i<8;$i++){
+          echo "mission1-".$i."<br>";
+        }
+        for($i=1;$i<6;$i++){
+          echo "mission2-".$i."<br>";
+        }
+        for($i=1;$i<9;$i++){
+          echo "mission3-".$i."<br>";
+        }
+        echo "mission4-1<br>";
+        echo "mission5-1<br>";
+        echo "mission6-1<br>";
+       ?>
+      <h1>ランキング</h1>
+    </div>
+  </body>
+</html>
